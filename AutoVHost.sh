0d58fdf511f1595b260f02035dc034f66d53652c
Shaswata Dutta <reedknight@localhost.localdomain>
2015-12-22 23:46:34 +0530

    Initial Commit

---
+++ AutoVHost.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# @Author : Shaswata Dutta
+# A script to automatically setup apache(httpd) virtual hosts
+# Currently developing only for apache 2.4 on Fedora 23
+
+# Invocation ./AutoVHost.sh test.example.com
+
+
